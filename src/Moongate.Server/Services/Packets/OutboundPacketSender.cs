f9433240bcaae74692a3437f47aaee53f167710c
tom <squid@stormwind.it>
2026-02-20 17:10:41 +0100

    feat: add synchronous Send method to IOutboundPacketSender

--- src/Moongate.Server/Services/Packets/OutboundPacketSender.cs
+++ src/Moongate.Server/Services/Packets/OutboundPacketSender.cs
@@ -21,6 +21,9 @@ public sealed class OutboundPacketSender : IOutboundPacketSender
     public OutboundPacketSender(MoongateConfig moongateConfig)
         => _logPacketData = moongateConfig.LogPacketData;
 
+    public bool Send(MoongateTCPClient client, OutgoingGamePacket outgoingPacket)
+        => SendAsync(client, outgoingPacket, CancellationToken.None).GetAwaiter().GetResult();
+
     public async Task<bool> SendAsync(
         MoongateTCPClient client,
         OutgoingGamePacket outgoingPacket,
