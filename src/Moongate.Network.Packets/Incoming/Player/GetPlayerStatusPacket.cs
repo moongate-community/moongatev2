bc80cf2ef4a69eb0fcc552914db5e4db94fa8c00
tom <squid@stormwind.it>
2026-02-21 09:25:09 +0100

    feat(network): parse get player status packet and add tests

--- src/Moongate.Network.Packets/Incoming/Player/GetPlayerStatusPacket.cs
+++ src/Moongate.Network.Packets/Incoming/Player/GetPlayerStatusPacket.cs
@@ -2,15 +2,33 @@ using Moongate.Network.Packets.Attributes;
 using Moongate.Network.Packets.Base;
 using Moongate.Network.Packets.Types.Packets;
 using Moongate.Network.Spans;
+using Moongate.UO.Data.Types;
 
 namespace Moongate.Network.Packets.Incoming.Player;
 
 [PacketHandler(0x34, PacketSizing.Fixed, Length = 10, Description = "Get Player Status")]
 public class GetPlayerStatusPacket : BaseGameNetworkPacket
 {
+    public uint MobileSerial { get; set; }
+
+    public GetPlayerStatusType StatusType { get; set; }
+
+    public uint UnknownPattern { get; set; }
+
     public GetPlayerStatusPacket()
         : base(0x34, 10) { }
 
     protected override bool ParsePayload(ref SpanReader reader)
-        => true;
+    {
+        if (reader.Remaining != 9)
+        {
+            return false;
+        }
+
+        UnknownPattern = reader.ReadUInt32();
+        StatusType = (GetPlayerStatusType)reader.ReadByte();
+        MobileSerial = reader.ReadUInt32();
+
+        return reader.Remaining == 0;
+    }
 }
